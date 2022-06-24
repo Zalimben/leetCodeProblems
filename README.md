b2befe5ab504c163f2e18b1037ca63b0ad9cedc3
Zalimben <szalimben93@gmail.com>
2022-06-24 11:16:51 -0400

    Solution: - Rotate Array - Squares Sorted Array
    Signed-off-by: Zalimben <szalimben93@gmail.com>

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ I will push [LeetCode](https://leetcode.com/) Solutions to this project
 ## Topics
 
 ### Algorithm
+#### Easy
 - [Binary Search Problem](src/main/java/leetCodeProblems/algorithm/BinarySearchProblem.java)
-- [First Bad Version]((src/main/java/leetCodeProblems/algorithm/BinarySearchProblem.java))
+- [First Bad Version](src/main/java/leetCodeProblems/algorithm/BinarySearchProblem.java)
 - [Search Insert Position](src/main/java/leetCodeProblems/algorithm/SearchInsertPosition.java)
-
+- [SquaresSortedArray](src/main/java/leetCodeProblems/algorithm/SquaresSortedArray.java)
+#### Medium
+- [RotateArray](src/main/java/leetCodeProblems/algorithm/RotateArray.java)
---
+++ src/main/java/leetCodeProblems/algorithm/RotateArray.java
@@ -0,0 +1,57 @@
+package main.java.leetCodeProblems.algorithm;
+
+/**
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ *
+ * Example 1:
+ * Input: nums = [1,2,3,4,5,6,7], k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ *
+ * Example 2:
+ * Input: nums = [-1,-100,3,99], k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ *
+ *
+ * Constraints:
+ * 1 <= nums.length <= 105
+ * -231 <= nums[i] <= 231 - 1
+ * 0 <= k <= 105
+ *
+ *
+ * Follow up:
+ * Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ */
+public class RotateArray
+{
+	public static void main(String[] args)
+	{
+		int[] nums = {1,2,3,4,5,6,7,8,9,10,12};
+		int step = 15;
+		rotateArray(nums, step);
+		for(int num : nums) {
+			System.out.print(num);
+		}
+	}
+
+	private static void rotateArray(int[] nums, int step)
+	{
+		int[] auxNums = new int[nums.length];
+		int newIdx;
+		for(int idx = 0; idx < nums.length; idx++) {
+			newIdx = idx + step;
+			while(newIdx >= auxNums.length ){
+				newIdx = newIdx - auxNums.length;
+			}
+			auxNums[newIdx] = nums[idx];
+		}
+		System.arraycopy(auxNums, 0, nums, 0, nums.length);
+	}
+}
