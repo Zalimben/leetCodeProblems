d77a8fa0628bbf049559e6b80393acde1fbc644a
Zalimben <szalimben93@gmail.com>
2022-06-27 11:58:52 -0400

    - Move Zeroes - Two Sum II
    Signed-off-by: Zalimben <szalimben93@gmail.com>

--- README.md
+++ README.md
@@ -10,5 +10,7 @@ I will push [LeetCode](https://leetcode.com/) Solutions to this project
 - [First Bad Version](src/main/java/leetCodeProblems/algorithm/BinarySearchProblem.java)
 - [Search Insert Position](src/main/java/leetCodeProblems/algorithm/SearchInsertPosition.java)
 - [SquaresSortedArray](src/main/java/leetCodeProblems/algorithm/SquaresSortedArray.java)
+- [Move Zeroes](src/main/java/leetCodeProblems/algorithm/MoveZeroes.java)
 #### Medium
-- [RotateArray](src/main/java/leetCodeProblems/algorithm/RotateArray.java)
+- [Rotate Array](src/main/java/leetCodeProblems/algorithm/RotateArray.java)
+- [Two Sum II - Input Array Is Sorted](src/main/java/leetCodeProblems/algorithm/TwoSum.java)
---
+++ src/main/java/leetCodeProblems/algorithm/MoveZeroes.java
@@ -0,0 +1,41 @@
+package main.java.leetCodeProblems.algorithm;
+
+/**
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * Note that you must do this in-place without making a copy of the array.
+ *
+ * Example 1:
+ * Input: nums = [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ *
+ * Example 2:
+ * Input: nums = [0]
+ * Output: [0]
+ *
+ * Constraints:
+ * 1 <= nums.length <= 104
+ * -231 <= nums[i] <= 231 - 1
+ *
+ * Follow up: Could you minimize the total number of operations done?
+ */
+public class MoveZeroes
+{
+	public static void main(String[] args)
+	{
+		int[] nums = {0,1,0,3,12};
+		moveZeroes(nums);
+	}
+
+	private static void moveZeroes(int[] nums)
+	{
+        int lastNonZeroFoundAt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+	}
+}
